0767ed0b9121dfc3bdf580fa00d4fd30fc395817
Jakub Valenta <jakub@jakubvalenta.cz>
2019-05-14 21:41:04 +0200

    Render the Graphviz file using Jinja

--- voting_advice_app_comparison/main.py
+++ voting_advice_app_comparison/main.py
@@ -5,13 +5,16 @@ import itertools
 import sys
 from dataclasses import dataclass
 from pathlib import Path
-from typing import IO, Iterator, List, Set
+from typing import IO, List, Set
+
+from jinja2 import Environment, PackageLoader
 
 
 @dataclass
 class Question:
     id_: str
     text: str
+    has_link: bool
 
 
 @dataclass
@@ -22,17 +25,17 @@ class App:
 
 @dataclass
 class Link:
-    left: str
-    right: str
-    same: bool
+    ids: List[str]
+    same: bool = False
+    dummy: bool = False
 
 
-def read_app(path: str) -> App:
+def read_app(path: str, q_ids_with_links: List[str]) -> App:
     lang = Path(path).stem
     with open(path) as f:
         reader = csv.reader(f)
         questions = [
-            Question(id_=id_, text=text_en)
+            Question(id_=id_, text=text_en, has_link=id_ in q_ids_with_links)
             for id_, text_orig, text_en in reader
         ]
     return App(lang=lang, questions=questions)
@@ -43,51 +46,41 @@ def read_links(path: str) -> List[Link]:
         reader = csv.reader(f)
         next(reader)
         return [
-            Link(left=left, right=right, same=int(same))
-            for left, right, same in reader
-            if left and right
+            Link(ids=row[1:], same=int(row[0]))
+            for row in reader
+            if all(row[1:])
         ]
 
 
-def find_question_ids_with_link(links: List[Link]) -> Set[str]:
-    return set(
-        itertools.chain.from_iterable(
-            (link.left, link.right) for link in links
-        )
-    )
-
-
-def format_graph(apps: List[App], links: List[Link]) -> Iterator[str]:
-    q_ids_with_links = find_question_ids_with_link(links)
-    yield 'digraph G {\n'
-    yield '    rankdir=LR; ranksep=3; nodesep=0.05;\n'
-    yield '    graph [pencolor=transparent,splines=line,fontname="Roboto"];\n'
-    yield '    node [shape=box,fontname="Roboto",width=13];\n'
-    yield '    edge [dir=none];\n'
-    for i, app in enumerate(apps):
-        yield f'    subgraph cluster{i} {{\n'
-        yield f'        label="{app.lang}";\n'
-        yield f'        rank=same;\n'
-        for q in app.questions:
-            style = ',style=filled' if q.id_ not in q_ids_with_links else ''
-            align = '\\r' if i == 0 else '\\l'
-            yield f'        {q.id_} [label="{q.text}{align}"{style}];\n'
-        yield '    }\n'
+def add_first_dummy_link(apps: List[App], links: List[Link]):
     first_questions = next(zip(*(app.questions for app in apps)))
-    first_ids = [q.id_ for q in first_questions]
-    first_link = ' -> '.join(first_ids)
-    yield f'    {first_link}[style=invis,weight=100];\n'
-    for link in links:
-        options = ' [style=dashed]' if not link.same else ''
-        yield f'    {link.left}:e -> {link.right}:w{options};\n'
-    yield '}\n'
+    first_ids = (q.id_ for q in first_questions)
+    first_link = Link(ids=first_ids, dummy=True)
+    links.insert(0, first_link)
+
+
+def find_question_ids_with_link(links: List[Link]) -> Set[str]:
+    return set(itertools.chain.from_iterable(link.ids for link in links))
+
+
+def render_template(package: List[str], f_out: IO, **context):
+    environment = Environment(loader=PackageLoader(*package[:-1]))
+    template = environment.get_template(package[-1])
+    stream = template.stream(**context)
+    f_out.writelines(stream)
 
 
 def create_and_write_graph(path_links: str, paths_apps: List[str], f_out: IO):
-    apps = [read_app(path) for path in paths_apps]
     links = read_links(path_links)
-    graph_lines = format_graph(apps, links)
-    f_out.writelines(graph_lines)
+    q_ids_with_links = find_question_ids_with_link(links)
+    apps = [read_app(path, q_ids_with_links) for path in paths_apps]
+    add_first_dummy_link(apps, links)
+    render_template(
+        ['voting_advice_app_comparison', 'templates', 'graph.gv'],
+        f_out,
+        apps=apps,
+        links=links,
+    )
 
 
 def main():
